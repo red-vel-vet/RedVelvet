32a1c4fcdf14267eb538b605acfc0f966542bd72
Raymond Willey <rwilley@Raymonds-MacBook-Pro.local>
2024-06-25 13:49:08 -0400

    modal formatting

--- frontend/src/components/EventDetails.jsx
+++ frontend/src/components/EventDetails.jsx
@@ -7,30 +7,40 @@ function EventDetails({ selectedEvent, eventModalVisible, onCancel }) {
 
     const eventDate = new Date(selectedEvent.start);
     const eventEndDate = new Date(selectedEvent.end);
-    const month = eventDate.toLocaleString('default', { month: 'short' });
-    const day = eventDate.getDate();
-    const dayName = eventDate.toLocaleString('default', { weekday: 'short' });
-    const startTime = eventDate.toLocaleTimeString([], { hour: 'numeric', minute: '2-digit' });
-    const endTime = eventEndDate.toLocaleTimeString([], { hour: 'numeric', minute: '2-digit' });
+    const month = eventDate.toLocaleString('default', { month: 'short', timeZone: 'UTC' });
+    const day = eventDate.getUTCDate();
+    const dayName = eventDate.toLocaleString('default', { weekday: 'short', timeZone: 'UTC' });
+    const startTime = eventDate.toLocaleTimeString([], { hour: 'numeric', minute: '2-digit', timeZone: 'UTC' });
+    const endTime = eventEndDate.toLocaleTimeString([], { hour: 'numeric', minute: '2-digit', timeZone: 'UTC' });
 
     const defaultImage = 'https://images.unsplash.com/photo-1571118027171-d2e2c56cc926?q=80&w=2371&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
 
     return (
         <div className="modal-background">
             <div className="modal-content">
-                <img src={logo} alt="Red Velvet Icon" className="event-icon" />
                 <div className="container">
-                    <h2 className="host">{selectedEvent.host.toUpperCase()}</h2>
-                    <p className="event-name">{selectedEvent.title}</p>
-                    <img src={selectedEvent.image_url || defaultImage} alt="Event" className="event-image" />
-                    <p className="details-text">{month} {day} {startTime} - {endTime}</p>
-                    <p className="details-text">{selectedEvent.city}, {selectedEvent.state}</p>
+                    <div className="modal-header">
+                        <img src={logo} alt="Red Velvet Icon" className="event-icon" />
+                        <div className="event-header">
+                            <p className="modal-host">{selectedEvent.host.toUpperCase()}</p>
+                            <p className="modal-event-name">{selectedEvent.title.toUpperCase()}</p>
+                            <p className="details-text">{dayName}, {month} {day} • {startTime} - {endTime}</p>
+                            <p className="details-text">{selectedEvent.city}, {selectedEvent.state}</p>
+                        </div>
+                    </div>
+                    
                     <div className="scroll-view-container">
                         <div className="scroll-view">
-                            <p className="text">
-                                {selectedEvent.description}
+                            <img src={selectedEvent.image_url || defaultImage} alt="Event" className="event-image" />
+                            <div className="text">
+                                {selectedEvent.description.split('\n').map((line, index) => (
+                                    <React.Fragment key={index}>
+                                    {line}
+                                    <br/><br/>
+                                    </React.Fragment>
+                                ))}
                                 {selectedEvent.membershipRequired ? 'Membership Required.' : ''}
-                            </p>
+                            </div>
                         </div>
                     </div>
                 </div>
--- frontend/src/components/EventItem.jsx
+++ frontend/src/components/EventItem.jsx
@@ -3,10 +3,10 @@ import '../styles/EventItem.css'; // Component-specific styles
 
 const EventItem = ({ event, onClick }) => {
     const eventDate = new Date(event.start);
-    const month = eventDate.toLocaleString('default', { month: 'short' });
-    const day = eventDate.getDate();
-    const dayName = eventDate.toLocaleString('default', { weekday: 'short' });
-    const startTime = eventDate.toLocaleTimeString([], { hour: 'numeric', minute: '2-digit' });
+    const month = eventDate.toLocaleString('default', { month: 'short', timeZone: 'UTC' });
+    const day = eventDate.getUTCDate();
+    const dayName = eventDate.toLocaleString('default', { weekday: 'short', timeZone: 'UTC' });
+    const startTime = eventDate.toLocaleTimeString([], { hour: 'numeric', minute: '2-digit', timeZone: 'UTC' });
 
     const defaultImage = 'https://images.unsplash.com/photo-1571118027171-d2e2c56cc926?q=80&w=2371&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
 
